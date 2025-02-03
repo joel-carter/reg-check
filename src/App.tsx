4d75018ca22e0a607d3809338337b62e50e4dffa
Joel Carter (UG) <b9017927@newcastle.ac.uk>
2025-02-03 23:07:24 +0000

    Add existing project files to Git

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import './App.css';
+import CarAgeLookup from './components/CarAgeLookup';
+
+function App() {
+  return (
+    <div className="App">
+      <CarAgeLookup />
+    </div>
+  );
+}
+
+export default App;
